603d34d400bbd6629cd47da2a14c1d98c0797341
Norberto <norbertoj54@hotmail.com>
2025-03-06 17:11:51 -0300

    Components for HTML created

---
+++ src/components/Body.js
@@ -0,0 +1,20 @@
+import logo from '../images/logo.svg';
+
+export default function Body() {
+  return (
+    <h2 className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+    </h2>
+  );
+}
