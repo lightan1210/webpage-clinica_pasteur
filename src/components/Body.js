b3844751d38ae73766811da61afe262ad987bfcd
Norberto <norbertoj54@hotmail.com>
2025-03-06 17:21:41 -0300

    clean Body and change title of page

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,20 +1,7 @@
-import logo from '../images/logo.svg';
-
 export default function Body() {
-  return (
-    <h2 className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </h2>
-  );
+    return (
+        <h2>
+            SOY UN BODY
+        </h2>
+    );
 }
