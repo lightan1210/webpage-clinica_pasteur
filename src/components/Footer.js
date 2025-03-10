f72cd27df4f89f36ba1a0d67e8835d84ac887054
Norberto <norbertoj54@hotmail.com>
2025-03-10 13:05:41 -0300

    Add Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,39 @@
+import instagram from '../images/instagram_icon.png'
+import facebook from '../images/facebook_icon.png'
+import linkedin from '../images/linkedin_icon.png'
+
 export default function Footer() {
     return (
-        <h3>
-            SOY UN FOOTER
-        </h3>
+        <footer>
+            <ul className="Info">
+                <li>
+                    <ul className="SubInfo">
+                        <label>Empresa</label>
+                        <li>Acerca de Nosotros</li>
+                    </ul>
+                </li>
+                <li>
+                    <ul className="SubInfo">
+                        <label>Contacto</label>
+                        <li>
+                            <a href="#">
+                                <img src={instagram} alt="Instagram"></img>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <img src={facebook} alt="Facebook"></img>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <img src={linkedin} alt="LinkedIn"></img>
+                            </a>
+                        </li>
+                        <li></li>
+                    </ul>
+                </li>
+            </ul>
+        </footer>
     );
 }
