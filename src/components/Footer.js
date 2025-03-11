eac470fdff8f6abf8400d09e11225e87df3c5ab9
Norberto <norbertoj54@hotmail.com>
2025-03-10 23:51:47 -0300

    Basic structure for footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,3 +1,6 @@
+import phone from '../images/phone_icon.png'
+import whatsapp from '../images/whatsapp_icon.png'
+
 import instagram from '../images/instagram_icon.png'
 import facebook from '../images/facebook_icon.png'
 import linkedin from '../images/linkedin_icon.png'
@@ -7,14 +10,31 @@ export default function Footer() {
         <footer>
             <ul className="Info">
                 <li>
-                    <ul className="SubInfo">
-                        <label>Empresa</label>
-                        <li>Acerca de Nosotros</li>
+                    <ul className='SubInfo'>
+                        <label>Solicite Un Turno</label>
+                        <li>
+                            <a>
+                                <img src={phone}/>
+                            </a>
+                            <label>0810 9999 911 o (0299) 4340000</label>
+                        </li>
+                        {/* <li>
+                            <a>
+                                <img src={phone}/>
+                            </a>
+                            <label>(0299) 4340000</label>
+                        </li> */}
+                        <li>
+                            <a>
+                                <img src={whatsapp}/>
+                            </a>
+                            <label>299 4532424 (solo mensajes)</label>
+                        </li>
                     </ul>
                 </li>
                 <li>
                     <ul className="SubInfo">
-                        <label>Contacto</label>
+                        <label>Redes Sociales</label>
                         <li>
                             <a href="#">
                                 <img src={instagram} alt="Instagram"></img>
@@ -30,7 +50,12 @@ export default function Footer() {
                                 <img src={linkedin} alt="LinkedIn"></img>
                             </a>
                         </li>
-                        <li></li>
+                    </ul>
+                </li>
+                <li>
+                    <ul className="SubInfo">
+                        <label>Empresa</label>
+                        <li>Acerca de Nosotros</li>
                     </ul>
                 </li>
             </ul>
