65e2f4487fde5eda2175f9727a5618f79a4acc24
Norberto <norbertoj54@hotmail.com>
2025-03-11 00:32:57 -0300

    Add Body with a Video Carrousel

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,7 +1,13 @@
 export default function Body() {
     return (
-        <h2>
-            SOY UN BODY
-        </h2>
+        <main>
+            <div className="VideosCarrousel">
+                <video controls muted playsInline src="https://video.wixstatic.com/video/abfa5a_8a396045e52c4d87a06053caf94dcf7e/1080p/mp4/file.mp4">
+                </video>
+                <p className="Epigraphy">
+                    Video conmemorativo por los 50 años de la Clínica Pasteur.
+                </p>
+            </div>
+        </main>
     );
 }
