204c9809f44d9c7cc73378f7a71eeac455d5e279
Norberto <norbertoj54@hotmail.com>
2025-03-11 16:52:16 -0300

    Install scss and switch from css

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Header from './components/Header.js'
 import Body from './components/Body.js'
 import Footer from './components/Footer.js'
 
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
