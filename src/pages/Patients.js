653c5342057baa3e1d8426bb3a4a8c99830e7031
Norberto <norbertoj54@hotmail.com>
2025-03-16 00:17:07 -0300

    Add page Patients

--- src/pages/Patients.js
+++ src/pages/Patients.js
@@ -1,7 +1,185 @@
 export default function Patients() {
     return(
-        <>
-        <h1>Información importante para pacientes</h1>
-        </>
+        <div className="Patients">
+            <h1>Información importante para pacientes</h1>
+            <h2>
+                ANTES DE INTERNARTE
+            </h2>
+            <b>
+                Queremos que estés bien
+            </b>
+            <p>
+                Queremos acompañarte en este momento de tu vida. Nuestra meta es cuidado integral de la salud con humanidad, excelencia médica y tecnología avanzada, con acompañamiento del paciente y su familia. Por eso, en cada paso de esta etapa queremos estar con vos, de modo que resulte lo más cómodo posible.
+            </p>
+            <b>
+                Queremos verte bien lo más pronto posible
+            </b>
+            <p>
+                Para ayudarte tenemos personas especialmente entrenadas cuyo trabajo es hacer todo más fácil para vos. Aquí vas a encontrar información útil. Léelo con cuidado y compartilo con tu familia.
+            </p>
+            <h2>
+                ¿Qué documentación necesitas para internarte?
+
+            </h2>
+            <ul>
+                <li>Documento válido.</li>
+                <li>Orden del médico.</li>
+                <li>Consentimiento informado ya firmado, si es que ya te lo dieron.</li>
+                <li>Carnet de la obra social o Prepaga.</li>
+                <li>Trae también todos los estudios previos que te hayan hecho, que llevaron a la internación actual.</li>
+            </ul>
+
+            <h2>
+                CUANDO ESTÁS INTERNADO
+            </h2>
+            <b>
+                Existimos para tu salud
+            </b>
+            <p>
+                Queremos que su tiempo en Clínica Pasteur sea lo más agradable y segura posible. Nos esforzamos cada día en brindar el mejor servicio. En Clínica Pasteur, nuestra misión es el cuidado integral de la salud con humanidad, excelencia médica y tecnología avanzada, con acompañamiento del paciente y su familia.
+            </p>
+
+            <b>
+                Sugerencias para la internación, para facilitar todo el proceso, te sugerimos que traigas estos elementos:
+            </b>
+
+            <ul>
+                <li>Ropa cómoda para cambiarte.</li>
+                <li>Recordá traer tus elementos de higiene personal, como por ejemplo: pasta dental y cepillo de dientes, shampoo.</li>
+            </ul>
+            <b>
+                Te pedimos que no traigas elementos de valor como:
+            </b>
+            <ul className="Not">
+                <li>Joyas</li>
+                <li>Celulares</li>
+                <li>Computadoras</li>
+                <li>Dinero en efectivo</li>
+                <li>Tarjetas de crédito</li>
+                <li>Tablets</li>
+            </ul>
+            <h2>
+                Horarios de visitas
+            </h2>
+            <ul>
+                <li>Si el paciente está en alguna habitación de los pisos 1, 2 o 3, el horario de visita es de 11 a 12 hs y de 18 a 20 hs.</li>
+                <li>Si el paciente está en Terapia intensiva o en Unidad Coronaria: el horario es de 12 a 12:30 hs y de 19:30 a 20, o cuando indiquen los médicos del sector.</li>
+            </ul>
+            <h3>
+                Horarios de visita a Neonatología
+            </h3>
+            <ul>
+                <li>Abuelos: miércoles y sábados de 11 a 12 horas.</li>
+                <li>Hermanos: miércoles y sábados de 16 a 17 horas.</li>
+            </ul>
+            <p>
+                <b>¡Importante!</b> El papá y la mamá no son considerados visitas. Papá y mamá no tienen ninguna restricción en los horarios, ¡siempre pueden entrar!
+            </p>
+            <p>
+                Las visitas son de aliento a las personas internadas. ¡Las alentamos y ayudamos a que se hagan de un modo ordenado para que el paciente obtenga el máximo beneficio! Por eso, recordá que el número máximo de visitas es de 2 por paciente, al mismo tiempo. Se pueden turnar durante el horario de visitas permitido. Los niños menores de 12 años no pueden ingresar. ¿Por qué? porque es una forma de cuidarlos. Ellos tienen más riesgos de contagios. Recordemos que hay otras personas internadas también, por eso, no se puede ingresar con alimentos o bebidas. Tampoco debemos permanecer ni hacer ruidos en los pasillos.
+
+            </p>
+            <h2>
+                Consejos para una mejor estadía en la Clínica
+            </h2>
+            <p>
+                En Clínica Pasteur buscamos que la estadía del paciente resulte lo más confortable posible. Recordemos que si una persona que está internada, lo hace por razones médicas, y eso determina el resto de las reglas que existen. Si el paciente no puede alimentarse por sí mismo, se le solicitará a un familiar que lo asista en los horarios de comida. Si el paciente trae estudios médicos previos, no olvide solicitarlos cuando reciba el alta y se retire. En Clínica Pasteur trabajamos para mantener los mejores niveles de seguridad, sin embargo, personas ajenas pueden aprovecharse del acceso semipúblico de una institución abierta. Por eso, sugerimos no traer objetos de valor (por ejemplo, computadoras, dinero en efectivo, celulares, etc.).
+            </p>
+            <b>
+                ¿Sabías que tu comportamiento ayuda a tu salud?
+            </b>
+            <ul>
+                <li>
+                    <b>
+                        ¡No olvides lavarte las manos! ¿Cuántas veces?
+                    </b>
+                    <ul>
+                        <li>Antes de entrar o después de salir de la habitación.</li>
+                        <li>Al tener contacto con equipos, otras personas, instalaciones u otras cosas.</li>
+                    </ul>
+                </li>
+                <li>
+                    <b>
+                        ¿Cómo lavarse las manos?
+                    </b>
+                    <ul>
+                        <li>Lavarse las manos no es mojarlas. Es un procedimiento que incluye jabón, agua y tiempo.</li>
+                        <li>¿Cuánto tiempo? Es fácil, canta la canción “Cumpleaños feliz” dos veces.</li>
+                    </ul>
+                </li>
+            </ul>
+            <b>
+                Preparate para cuando veas a un médico:
+            </b>
+            <ul>
+                <li>Anotá preguntas que se te ocurran cuando no estás con el médico</li>
+                <li>Preguntá sobre qué comer y cómo vestirte</li>
+                <li>Preguntá sobre qué movimientos puede hacer y cuáles no</li>
+                <li>Preguntá cómo tu familia puede ayudar</li>
+                <li>Preguntá sobre los medicamentos: cuáles vas a necesitar y cómo tomarlos</li>
+            </ul>
+
+            <b>
+                ¡No te olvides!
+            </b>
+            <p>
+                En el caso de las biopsias de pacientes intervenidos, deberán ser retiradas por el paciente y/o familiar dentro de las 72 horas posteriores a la toma (biopsia o pieza quirúrgica) para ser llevada a Anatomía Patológica. Pasado ese tiempo (72 horas) Clínica Pasteur no se hará responsable de la muestra.
+            </p>
+            <h2>
+                Acompañantes de personas internadas
+            </h2>
+            <p>
+                Los pacientes de los pisos 1, 2 y 3 o de pediatría pueden tener un acompañante. La persona que se queda con el paciente internado no puede usar la cama contigua aunque esté desocupada.
+            </p>
+            <p>
+                Los familiares van a recibir información de la evolución paciente en horario de 9:30 a 11:30 horas.
+            </p>
+            <p>
+                En caso de pacientes que tuvieron una intervención quirúrgica, pueden pactar un horario con el cirujano.
+            </p>
+            <p>
+                No puede usar el ascensor. Está reservado exclusivamente para aquellas personas condiscapacidad o dificultades para moverse por sus propios medios.
+            </p>
+            <b>
+                ¡Cuidamos el lugar entre todos!
+            </b>
+            <p>
+                Para seguridad de todos, no se permite el ingreso de vendedores ambulantes o personas no relacionadas con pacientes.
+            </p>
+            <h2>
+                Derechos de los pacientes
+            </h2>
+            <ul>
+                <li>Recibir tratamiento respetuoso en todo momento y bajo toda circunstancia.</li>
+                <li>Usar vestimenta personal apropiada, así como objetos religiosos simbólicos, siempre que no interfieran en los procedimientos de diagnóstico y/o tratamiento.</li>
+                <li>Ser examinado en un ámbito adecuado que asegure aislamiento visual y auditivo razonable.</li>
+                <li>Esperar que toda consulta o mención de su caso sea hecho discretamente y que no haya gente presente que no esté directamente involucrada en su tratamiento, sin su consentimiento.</li>
+                <li>A saber la identidad y la posición profesional de os individuos que le están prestando servicios, así como a saber qué médico o profesional de la salud está principalmente a cargo de su tratamiento. Esto incluye su participación voluntaria en programas de entrenamiento clínico.</li>
+                <li>A obtener información del profesional responsable de la coordinación de su tratamiento, respecto de su diagnóstico (hasta donde se sepa), tratamiento o diagnóstico. Dicha información deberá ser transmitida en un lenguaje de su absoluta comprensión.</li>
+                <li>A ser vigilado por personas ajenas a la institución y por él autorizadas.</li>
+                <li>A consultar a un especialista bajo petición y a su cuenta.</li>
+                <li>A rehusar tratamiento hasta donde se lo permita la ley. Cuando la denegación del tratamiento por el paciente o su representante autorizado legalmente impida la prestación del tratamiento adecuado según las normas profesionales la relación con el paciente se dará por terminada previo acuerdo.</li>
+            </ul>
+            <h2>
+                CÓMO SACAR UN TURNO:
+            </h2>
+            <p>
+                Por teléfono: 0810-9999-911
+            </p>
+            <h3>
+                DÓNDE ENCONTRARNOS:
+            </h3>
+            <ul>
+                <li>Sede Central e Internación: Rioja 36, Neuquén</li>
+                <li>Centro de Atención Integral: Juan B Justo 299, Neuquén</li>
+                <li>Centro de Atención Integral: Roca 109, Neuquén</li>
+                <li>Centro de Atención Integral: Rufino Ortega 130, Neuquén</li>
+                <li>Cardiología: JB Justo 290, Neuquén</li>
+                <li>Centro de Atención Integral: San Martín 355, Neuquén</li>
+                <li>Centro de Diagnóstico por Imágenes: Rioja 36, Neuquén</li>
+                <li>Servicio de Oftalmología: Rioja 28, Neuquén</li>
+                <li>Centro de Rehabilitación Pasteur: Alaska 6571, Neuquén</li>
+            </ul>
+        </div>
     )
 }
